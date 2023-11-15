30ace35960e65654cf2a4533abefb4e36cd7c815
abelhubArchi <bolevel12@gmail.com>
2023-11-15 13:48:45 -0400

    Esta es la produccion

--- base/modles/index.js
+++ base/modles/index.js
@@ -1,71 +1,93 @@
 import clipboardy from 'clipboardy';
+import fs from 'fs';
 import notifier from 'node-notifier';
 import OpenAI from "openai";
-// Importa la librería dotenv
-import fs from 'fs';
 
+async function main() {
+  try {
+    // Leer datos desde el archivo
+    const data = fs.readFileSync('datos.json', 'utf-8');
+    const datos = JSON.parse(data);
 
+    const openai = new OpenAI({
+      apiKey: datos.llave,
+    });
 
-//fs.readFile('./datos.json', 'utf-8', (error, data)=>{
-fs.readFile('datos.json', 'utf-8', (error, data)=>{
-  const datos = JSON.parse(data)
+    if (datos.identificacion == 3132) {
+      async function consulta(pregunta) {
+        try {
+          const response = await openai.chat.completions.create({
+            model: "gpt-4-1106-preview",
+            messages: [
+              {"role": "system", "content": `Responderas estas preguntas de 
+                este examen de derecho de forma
+                rapida, breve y consisa,
+                Eligiendo con tu lógica y conocimiento unas de las respuestas
+                y si no hay respuestas solo dame la respuesta  correcta. 
+                Tu misión es pasar este examen con el mayor puntaje posible
+                estamos en Bolivia`},
+              {"role": "user","content": pregunta},
+            ],
+            temperature: 0,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+          });
 
-   const openai = new OpenAI({
-    apiKey: datos.llave,
-  });
+          notifier.notify({
+            title: 'Morgan',
+            message: response.choices[0].message.content
+          });
+          console.log("respuesta es: " + response.choices[0].message.content);
+        } catch (error) {
+          console.error("Error en la consulta:", error);
+        }
+      }
 
-  if (datos.identificacion == 1) {
-    async function consulta(pregunta) {
-      const response = await openai.chat.completions.create({
-        model: "gpt-4-1106-preview",
-        messages: [
-          {"role": "system", "content": `Responderas estas preguntas de 
-          este examen de derecho de forma
-          rapida, breve y consisa,
-          Eligiendo con tu logica y conocimiento unas de las respuestas
-           y si no hay respuestas solo dame la respuesta  correcta. 
-          Tu mision es pasar este examen con el mayor puntaje posible
-          estamos en Bolivia`},
-          {"role": "user","content": pregunta},
-        ],
-        temperature: 0,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-    
-      notifier.notify({
-        title: 'Morgan',
-        message: response.choices[0].message.content
-      });
-      console.log("respuesta es: " + response.choices[0].message.content);
-    }
-    var previousClipboardData = clipboardy.readSync();
-    function checkClipboard() {
+      var previousClipboardData = clipboardy.readSync();
+      function checkClipboard() {
         const currentClipboardData = clipboardy.readSync();
         if (currentClipboardData !== previousClipboardData) {
-            console.log("texto copiad:", currentClipboardData);
-              // Puedes mostrar una notificación
-            consulta(currentClipboardData)
-            previousClipboardData = currentClipboardData;
-        };
+          console.log("texto copiado:", currentClipboardData);
+          // Puedes mostrar una notificación
+          consulta(currentClipboardData)
+          previousClipboardData = currentClipboardData;
+        }
+      }
+
+      // Verifica el portapapeles cada segundo
+      setInterval(checkClipboard, 500);
+
+      console.log('La aplicación está en ejecución en segundo plano... Copia las preguntas y el sistema te enviará una respuesta por notificación.');
+    } else {
+      console.log('Aplicación en modo prueba');
+
+      var previousClipboardData = clipboardy.readSync();
+      function checkClipboard() {
+        const currentClipboardData = clipboardy.readSync();
+        if (currentClipboardData !== previousClipboardData) {
+          console.log("texto copiado:", currentClipboardData);
+          // Puedes mostrar una notificación
+          previousClipboardData = currentClipboardData;
+
+          notifier.notify({
+            title: 'Morgan',
+            message: 'Esta Sera La Respuesta'
+          });
+          console.log("Respuesta");
+        }
+      }
+
+      // Verifica el portapapeles cada segundo
+      setInterval(checkClipboard, 500);
     }
-    
-    
-    // Verifica el portapapeles cada segundo
-    setInterval(checkClipboard, 500);
-    
-    console.log('La aplicación está en ejecución en segundo plano... Copia las preguntas y el sistema te enviara una respuesta por notificacion.');
-    
-  } else{
-    console.log('Codigo de usuario incorrecto. Ingrese un codigo valido');
-    return;
+  } catch (error) {
+    console.error("Error general:", error);
+    // Puedes reiniciar el programa aquí, por ejemplo, llamando a la función main nuevamente
+    main();
   }
-    
-})
+}
 
-
-
-
-
+// Llamada inicial a la función main
+main();
